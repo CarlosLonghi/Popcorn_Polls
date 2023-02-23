d021aaa443c2c661f2bc91720a14e4459943f898
carlos <carloslonghi.cl@gmail.com>
2023-02-23 17:50:47 -0300

    style: create theme

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#0D0C0F',
+    BACKGROUND_800: '#1C1B1E',
+    BACKGROUND_700: '#262529',
+
+    RED_LIGHT: '#FF4F4F',
+    WINE: '#A70000',
+
+    GRAY_100: '#CAC4CF',
+    GRAY_300: '#948F99',
+
+    RED: '#FF0000',
+    GREEN: '#00D501'
+  }
+}
