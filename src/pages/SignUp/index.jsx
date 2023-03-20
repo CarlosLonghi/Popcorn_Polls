52117f20f353e0c4811b9a4574128e18493d7d12
carlos <carloslonghi.cl@gmail.com>
2023-03-20 16:13:59 -0300

    feat: create SignUp user

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,50 @@
+import { useState } from 'react';
 import { FiUser, FiMail, FiLock, FiArrowLeft} from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-
-import { Container, Form, Background } from "./styles";
 import { ButtonText } from '../../components/ButtonText';
 
+import { Container, Form, Background } from "./styles";
+
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  const navigate = useNavigate()
+  
+  const passwordMatched = password === passwordConfirm
+  const checkFields = !name || !email || !password || !passwordConfirm
+  
+  function handleSignUp(){
+    if(checkFields){
+      return alert('Preencha todos os campos!')
+    }
+
+    if(!passwordMatched){
+      return alert('As senhas não correspondem!')
+    }
+
+    api.post('/users', {name, email, password})
+    .then(() => {
+      alert('Usuário cadastrado com sucesso!')
+      navigate(-1)
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message)
+      }else {
+        alert('Não foi possível cadastrar!')
+      }
+    })
+    
+  }
+
   return (
     <Container>
       <Form>
@@ -22,30 +59,37 @@ export function SignUp() {
           placeholder='Nome'
           type='text'
           icon={FiUser}
+          onChange={event => setName(event.target.value)}
         />
         <Input
           placeholder='E-mail'
           type='text'
           icon={FiMail}
+          onChange={event => setEmail(event.target.value)}
         />
 
         <Input
           placeholder='Senha'
           type='password'
           icon={FiLock}
+          onChange={event => setPassword(event.target.value)}
         />
         <Input
           placeholder='Confirmar Senha'
           type='password'
           icon={FiLock}
+          onChange={event => setPasswordConfirm(event.target.value)}
         />
 
-        <Button title='Cadastrar'/>
+        <Button 
+          title='Cadastrar'
+          onClick={handleSignUp}
+        />
 
         <ButtonText 
           title='Voltar para o login'
           icon={FiArrowLeft}
-          to='/'
+          to={-1}
         />
 
       </Form>
