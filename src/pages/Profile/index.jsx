44dab0ecc3fb4c15bef57b599e67cecb842d1092
carlos <carloslonghi.cl@gmail.com>
2023-03-21 18:33:24 -0300

    feat: create update user datas in Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
+
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera, FiSave } from 'react-icons/fi'
 import { GiPopcorn } from 'react-icons/gi'
+
+import { useAuth } from '../../hooks/auth'
 
 import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
@@ -8,6 +12,24 @@ import { Input } from '../../components/Input'
 import { Container, Form, Avatar, Background } from "./styles";
 
 export function Profile(){
+  const { user, updateProfile } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate() {
+    const user = {
+      name, 
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+    await updateProfile({ user })
+  }
+
   return(
     <Container>
       <header>
@@ -40,27 +62,34 @@ export function Profile(){
           placeholder='Nome'
           type='text'
           icon={FiUser}
+          value={name}
+          onChange={event => setName(event.target.value)}
         />
         <Input
           placeholder='E-mail'
           type='text'
           icon={FiMail}
+          value={email}
+          onChange={event => setEmail(event.target.value)}
         />
 
         <Input
           placeholder='Senha Atual'
           type='password'
           icon={FiLock}
+          onChange={event => setPasswordOld(event.target.value)}
         />
         <Input
           placeholder='Nova Senha'
           type='password'
           icon={FiLock}
+          onChange={event => setPasswordNew(event.target.value)}
         />   
 
         <Button
           title='Salvar'
           icon={FiSave}
+          onClick={handleUpdate}
         />
       </Form>
 
