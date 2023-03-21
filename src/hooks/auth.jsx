fb95369c6e3d1df8f46e6437eb892b34350db5d8
carlos <carloslonghi.cl@gmail.com>
2023-03-20 22:58:48 -0300

    feat: finish SignIn using LocalStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from '../services/api'
 
@@ -10,10 +10,13 @@ function AuthProvider({ children }) {
   async function signIn({ email, password }) {
     try {
       const response = await api.post('/sessions', { email, password })
-      const { user, token } = response.data
+      const { token, user } = response.data
+
+      localStorage.setItem('@popcorn_polls:token', token)
+      localStorage.setItem('@popcorn_polls:user', JSON.stringify(user))
 
       api.defaults.headers.authorization = `Bearer ${token}`
-      setData({user, token})
+      setData({ token, user})
 
     } catch (error) {
       if (error.response) {
@@ -23,6 +26,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@popcorn_polls:token')
+    const user = localStorage.getItem('@popcorn_polls:user')
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  },[])
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
